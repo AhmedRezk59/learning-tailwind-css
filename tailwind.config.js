f5559a6514b3ab65acd458746e31e18ef65b9f88
Ahmed Mohamed Rizk <rezk59315@gmail.com>
2022-02-03 17:59:39 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+      body: ['Overpass' , 'sans-serif']
+      },
+      colors: {
+        primary:'#303669'
+      },
+      spacing: {
+        112:'28rem',
+        128:'32rem',
+      }
+    },
+    
+  },
+  plugins: [],
+}
